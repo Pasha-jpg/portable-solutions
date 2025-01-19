1bb138666f13b38f0aa5ac2a21d2fc4aed373587
pasha <70097930+Pasha-jpg@users.noreply.github.com>
2025-01-19 13:02:06 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,25 @@
+var myIndex = 0;
+		carousel();
+
+		function carousel() {
+		  var i;
+		  var x = document.getElementsByClassName("mySlides");
+		  for (i = 0; i < x.length; i++) {
+		    x[i].style.display = "none";  
+		  }
+		  myIndex++;
+		  if (myIndex > x.length) {myIndex = 1}    
+		  x[myIndex-1].style.display = "block";  
+		  setTimeout(carousel, 3000);    
+		}
+
+
+/*mobile-nav-*/
+function myFunction() {
+        var x = document.getElementById("myLinks");
+        if (x.style.display === "block") {
+          x.style.display = "none";
+        } else {
+          x.style.display = "block";
+        }
+      }
